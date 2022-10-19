ef9e2889a2d2fa1903299f3c96435edfac9b3bd5
cczyWyc <274230928@qq.com>
2022-10-20 00:24:00 +0800

    paxos-go: add proposal and acceptor

--- paxos/acceptor.go
+++ paxos/acceptor.go
@@ -1,1 +1,62 @@
 package paxos
+
+import (
+	"errors"
+	"fmt"
+	"log"
+)
+
+type Acceptor struct {
+	accepted map[string]*Proposal
+	promised map[string]*Proposal
+}
+
+type AcceptorClientInterface interface {
+	GetName() string
+	SendPrepare(proposal *Proposal) (*Proposal, error)
+	SendPropose(proposal *Proposal) (*Proposal, error)
+}
+
+// NewAcceptor create a new acceptor instance
+func NewAcceptor() *Acceptor {
+	return &Acceptor{
+		accepted: make(map[string]*Proposal),
+		promised: make(map[string]*Proposal),
+	}
+}
+
+// ReceivePrepare If an acceptor receives a prepare request with number N greater than that of
+// any prepare request to which it has already response, then it responds to the request with a promise not to accept
+// any more proposals numbered less than N and with the highest-numbered proposal (if any) that it has accepted.
+func (a *Acceptor) ReceivePrepare(proposal *Proposal) (*Proposal, error) {
+	promised, ok := a.promised[proposal.Key]
+	if ok && promised.Number > proposal.Number {
+		return nil, errors.New(fmt.Sprintf("Already promised to accept %s which is > than requested %s",
+			promised, proposal))
+	}
+
+	// Promise to accept the proposal
+	a.promised[proposal.Key] = proposal
+	log.Printf("Promised to accept %s", proposal)
+
+	return proposal, nil
+}
+
+// ReceivePropose If an acceptor receive a propose request for a proposal numbered N, it accepts the proposal unless
+// it has already responded to a prepare request having a number greater than N
+func (a *Acceptor) ReceivePropose(proposal *Proposal) (*Proposal, error) {
+	promised, ok := a.promised[proposal.Key]
+	if ok && promised.Number > proposal.Number {
+		return nil, errors.New(fmt.Sprintf("Already promised to accept %s which is > than requested %s",
+			promised, proposal))
+	}
+
+	// accept the proposal
+	a.accepted[proposal.Key] = proposal
+	log.Printf("Accepted %s", proposal)
+
+	// Truncate promises map
+	a.promised = make(map[string]*Proposal)
+
+	return proposal, nil
+}
--- paxos/proposal.go
+++ paxos/proposal.go
@@ -1,1 +1,13 @@
 package paxos
+
+import "fmt"
+
+type Proposal struct {
+	Number int
+	Key    string
+	Value  []byte
+}
+
+func (p *Proposal) String() string {
+	return fmt.Sprintf("(num=%d, key=\"%s\", value=\"%s\")", p.Number, p.Key, p.Value)
+}
