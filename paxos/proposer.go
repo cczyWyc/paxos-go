e66bc7f3b4941e9e0c8e622bb4a314fbee1289c6
cczyWyc <274230928@qq.com>
2022-11-12 18:56:32 +0800

    paxos: add proposer

--- paxos/proposer.go
+++ paxos/proposer.go
@@ -1,5 +1,7 @@
 package paxos
 
+import "log"
+
 // Proposer proposes new value to acceptors
 type Proposer struct {
 	acceptorClients  []AcceptorClientInterface
@@ -18,9 +20,77 @@ func NewProposer(acceptorClients []AcceptorClientInterface) *Proposer {
 	}
 }
 
-// majority return simple majority of acceptor nodes
-func (p *Proposer) majority() int {
-	return len(p.acceptorClients)/2 + 1
+// Propose send a proposal request to the peers
+func (p *Proposer) Propose(proposal *Proposal) error {
+	// step1: prepare proposals until majority is reached
+	for !p.majorityReached(proposal) {
+		if err := p.prepare(proposal); err != nil {
+			return err
+		}
+	}
+	log.Printf("Reached majority %d", p.majority())
+
+	// step2: Propose the value agreed on by majority of acceptors
+	return p.propose(proposal)
+}
+
+// A proposer chooses a new proposal number n and sends a request to
+// each member of some set of acceptors, asking it to respond with:
+// (a) A promise never again to accept a proposal numbered less than n, and
+// (b) The proposal with the highest number less than n that it has accepted, if any.
+func (p *Proposer) prepare(proposal *Proposal) error {
+	proposal.Number++
+
+	log.Printf("Proposing %s", proposal)
+
+	for i := 0; i < p.majority(); i++ {
+		acceptorClient := p.acceptorClients[i]
+		promise, err := acceptorClient.SendPrepare(proposal)
+		if err != nil {
+			continue
+		}
+
+		// Get the previous promise
+		previousPromise, ok := p.acceptorPromises[acceptorClient.GetName()][proposal.Key]
+
+		// Previous promise is equal or greater than the new proposal, continue
+		if ok && previousPromise.Number >= promise.Number {
+			continue
+		}
+
+		// Save the new promise
+		p.acceptorPromises[acceptorClient.GetName()][proposal.Key] = promise
+
+		// Update the proposal to the one with bigger number
+		if promise.Number > proposal.Number {
+			log.Printf("Updating the proposal to %s", promise)
+			proposal = promise
+		}
+	}
+	return nil
+}
+
+// If the proposer receives the request responses from a majority of
+// the acceptors, then it can issue a proposal with number n and value
+// v, where v is the value of the highest-numbered proposal among the
+// responses, or is any value selected by the proposer if the responders
+// reported no proposals
+func (p *Proposer) propose(proposal *Proposal) error {
+	for _, acceptorClient := range p.acceptorClients {
+		accepted, err := acceptorClient.SendPropose(proposal)
+		if err != nil {
+			continue
+		}
+
+		log.Printf("%s has accepted %s", acceptorClient.GetName(), accepted)
+	}
+
+	// Truncate acceptor promises map
+	for _, acceptorClient := range p.acceptorClients {
+		p.acceptorPromises[acceptorClient.GetName()] = make(map[string]*Proposal)
+	}
+
+	return nil
 }
 
 // majorityReached returns true if number of matching promises from acceptors is equal or greater than simple majority
@@ -41,4 +111,9 @@ func (p *Proposer) majorityReached(proposal *Proposal) bool {
 		}
 	}
 	return marches >= p.majority()
+}
+
+// majority return simple majority of acceptor nodes
+func (p *Proposer) majority() int {
+	return len(p.acceptorClients)/2 + 1
 }
